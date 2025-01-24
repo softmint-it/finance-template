35abed9422efa7d4108b16a98890a5712d257a97
IT21259302-Narasinghe <nisalnarasinghe@gmail.com>
2025-01-24 14:31:44 +0530

    seo blog pages

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
 protected $table = 'blogs';
-protected $fillable = ['title', 'slug', 'meta_description','meta_keywords','content', 'temp_img', 'main_img', 'category', 'status','custom_page','published_at','url'];
+protected $fillable = ['title', 'slug', 'author','meta_description','meta_keywords','content', 'temp_img', 'main_img', 'category', 'status','custom_page','published_at','url'];
 
 protected $casts = [
     'published_at' => 'datetime',
